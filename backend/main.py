6e3fb3c85e100508372385d8020cc176a7038e10
MadaraRU <78321649+MadaraRU@users.noreply.github.com>
2023-02-03 22:11:04 +0100

    fixing add blog api

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson.json_util import dumps
 from datetime import datetime
-import json
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-client = MongoClient("mongodb+srv://mk_aloui:blog_elyadata@cluster0.frssgm7.mongodb.net/?retryWrites=true&w=majority")
+origins = [
+    "http://localhost:4200",
+    "http://localhost",
+    "http://localhost:8000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client = MongoClient(
+    "mongodb+srv://mk_aloui:blog_elyadata@cluster0.frssgm7.mongodb.net/?retryWrites=true&w=majority")
 db = client["blog_db"]
 collection = db["blog"]
 
@@ -19,34 +32,44 @@ async def get_blogs():
    # Convert each BSON document to a Python dictionary
     blogs = [dict(blog) for blog in result]
     # Remove unwanted fields and convert ObjectId to string representation
-    blogs = [{k: str(v) if isinstance(v, ObjectId) else v for k, v in blog.items() if not k.startswith("$")} for blog in blogs]
+    blogs = [{k: str(v) if isinstance(v, ObjectId) else v for k,
+              v in blog.items() if not k.startswith("$")} for blog in blogs]
     return blogs
 
 
 @app.get("/api/blogs/{blog_id}")
 async def get_blog(blog_id: str):
-    result = collection.find_one({"_id": ObjectId(blog_id)})
-    if result:
-        return result
+    blog = collection.find_one({"_id": ObjectId(blog_id)})
+    if blog:
+        blog["_id"] = str(blog["_id"])
+        return blog
     else:
         return {"message": "Blog not found"}
 
+
 @app.post("/api/blogs")
-async def add_blog(title: str, content: str, author: str):
+async def add_blog(
+    title: str = Body(..., embed=True),
+    author: str = Body(..., embed=True),
+    content: str = Body(..., embed=True)
+):
     blog = {
         "title": title,
-        "content": content,
         "author": author,
-        "created_at":datetime.utcnow()
-
+        "content": content,
+        "created_at": datetime.utcnow(),
+        "upvotes": 0,
+        "downvotes": 0
     }
-    result = collection.insert_one(blog)
-    return {"message": "Blog created successfully!", "blog_id": str(result.inserted_id)}
+
+    collection.insert_one(blog)
+    return {"message": "Blog created successfully!"}
 
 
 @app.put("/blogs/{blog_id}/upvote")
 async def upvote_blog(blog_id: str):
-    result = collection.update_one({"_id": ObjectId(blog_id)}, {"$inc": {"upvotes": 1}})
+    result = collection.update_one({"_id": ObjectId(blog_id)}, {
+                                   "$inc": {"upvotes": 1}})
     if result.modified_count:
         return {"message": "Blog upvoted successfully!"}
     else:
@@ -55,7 +78,8 @@ async def upvote_blog(blog_id: str):
 
 @app.put("/blogs/{blog_id}/downvote")
 async def downvote_blog(blog_id: str):
-    result = collection.update_one({"_id": ObjectId(blog_id)}, {"$inc": {"downvotes": 1}})
+    result = collection.update_one({"_id": ObjectId(blog_id)}, {
+                                   "$inc": {"downvotes": 1}})
     if result.modified_count:
         return {"message": "Blog downvoted successfully!"}
     else:
